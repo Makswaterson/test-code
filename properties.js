16203a80993f7a957e6852b2c1587200b936ee78
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-10-24 17:33:29 +0300

    add

--- properties.js
+++ properties.js
@@ -1,2 +1,5 @@
 const titleRef = document.querySelector('.js-hero__title');
-console.log(titleRef);
+titleRef.textContent = 'Title from JS';
+titleRef.style.fontSize = '50px';
+titleRef.style.color = 'tomato';
+console.log(titleRef.textContent);
