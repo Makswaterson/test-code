ab692f935c3a5fb66b43df0f67e1b41402a63f5c
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-10-26 01:42:57 +0300

    add card of gallery

---
+++ card-of-gallery.js
@@ -0,0 +1,11 @@
+'use strict';
+/*
+<li class="gallery-item">
+  <a href="#">
+    <img src="https://picsum.photos/id/237/200/300" alt="Labrador">
+  </a>
+</li>
+*/
+const listRef = document.querySelector('.js-gallery');
+const itemEl = document.createElement('li');
+listRef.append(itemEl);
