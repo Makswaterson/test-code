9ee8de7a9855a4f670c097fae93b416f04c71e15
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-10-25 23:50:11 +0300

    add

--- properties.js
+++ properties.js
@@ -3,3 +3,4 @@ titleRef.textContent = 'Title from JS';
 titleRef.style.fontSize = '50px';
 titleRef.style.color = 'tomato';
 console.log(titleRef.textContent);
+// titleRef.style.cssText = "color: blue; font-size: 40px; position: absolute";
