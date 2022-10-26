4236c5a858273885bc80937c6ca70edfc802f26f
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-10-26 23:57:35 +0300

    add card of gallery

--- card-of-gallery.js
+++ card-of-gallery.js
@@ -8,4 +8,15 @@
 */
 const listRef = document.querySelector('.js-gallery');
 const itemEl = document.createElement('li');
+itemEl.classList.add('gallery-item');
+const linkRef = document.createElement('a');
+linkRef.href = '#';
+
+const imgRef = document.createElement('img');
+imgRef.src = 'https://picsum.photos/id/237/200/300';
+imgRef.alt = 'Labrador';
+linkRef.append(imgRef);
+itemEl.append(linkRef);
+
 listRef.append(itemEl);
+console.log(listRef);
