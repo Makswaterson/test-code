19a551dfb42173a420e777c633fb24b495220029
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-05 01:42:58 +0200

    add

--- forms-events.js
+++ forms-events.js
@@ -11,8 +11,22 @@
 const formRef = document.querySelector('.js-contact-form');
 const onHandleSubmit = event => {
   event.preventDefault();
-  console.log('email', event.target.elements.email.value);
-  console.log('name', event.target.elements.name.value);
+  const {
+    elements: { name, email },
+  } = event.currentTarget;
+  if (name.value === '' || email.value === '') {
+    alert('Заповність будь ласка всі поля');
+    return;
+  }
+  const userData = {
+    name: name.value,
+    email: email.value,
+  };
+  console.log(userData);
+  //   console.log(name.value);
+  //   console.log(email.value);
+  //   console.log('email', event.currentTarget.elements.email.value);
+  //   console.log('name', event.currentTarget.elements.name.value);
 };
 
 formRef.addEventListener('submit', onHandleSubmit);
