a19714b46e3b310b4c2150cf98b471033c930c0a
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-04 18:11:17 +0200

    add css

--- forms-events.js
+++ forms-events.js
@@ -7,3 +7,12 @@
 //     name: 'Mango'
 //     email: 'mango@gmail.com'
 //  }
+
+const formRef = document.querySelector('.js-contact-form');
+const onHandleSubmit = event => {
+  event.preventDefault();
+  console.log('email', event.target.elements.email.value);
+  console.log('name', event.target.elements.name.value);
+};
+
+formRef.addEventListener('submit', onHandleSubmit);
