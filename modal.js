01b449a0ffb39c8e4798821c02c358d5cd0e4f20
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-05 23:04:57 +0200

    add modal window

--- modal.js
+++ modal.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const openModalBtn = document.querySelector('.js-modal-open');
+const backdropRef = document.querySelector('.js-backdrop');
+const closeModalBtn = document.querySelector('.js-modal-close');
+
+const showModal = event => {
+  backdropRef.classList.toggle('is-open');
+};
+
+const openBtnClick = event => {
+  showModal();
+  document.addEventListener('keydown', KeyBoardPress);
+};
+
+const closeBtnClick = event => {
+  showModal();
+  document.removeEventListener('keydown', KeyBoardPress);
+};
+
+openModalBtn.addEventListener('click', openBtnClick);
+
+closeModalBtn.addEventListener('click', closeBtnClick);
+
+const onBackDropClick = event => {
+  if (event.target === event.currentTarget) {
+    closeBtnClick();
+  }
+  //   console.log(event.target);
+  //   console.log(event.currentTarget);
+};
+
+backdropRef.addEventListener('click', onBackDropClick);
+
+function KeyBoardPress(event) {
+  if (event.key === 'Escape') {
+    closeBtnClick();
+  }
+  //   console.log(event.key);
+  //   console.log(event.code);
+}
