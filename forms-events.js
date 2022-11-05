5896f38f4c824191674ba5c2526a339ce7117655
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-05 13:02:38 +0200

    add form-elem

--- forms-events.js
+++ forms-events.js
@@ -9,6 +9,7 @@
 //  }
 
 const formRef = document.querySelector('.js-contact-form');
+const nameInputRef = document.querySelector('.js-username-input');
 const onHandleSubmit = event => {
   event.preventDefault();
   const {
@@ -30,3 +31,36 @@ const onHandleSubmit = event => {
 };
 
 formRef.addEventListener('submit', onHandleSubmit);
+
+const onInputFocus = event => {
+  console.log('focus');
+  console.log(event.target);
+  event.target.classList.add('focus');
+};
+nameInputRef.addEventListener('focus', onInputFocus);
+
+const onInputBlur = event => {
+  const { dataset, value } = event.target;
+  const requiredLength = Number(dataset.length);
+
+  event.target.classList.remove('focus');
+  // console.log('blur');
+  // console.log(requiredLength);
+  // console.log(value.length);
+  // console.log(requiredLength === value.length);
+  if (requiredLength === value.length) {
+    // event.target.classList.add('valid');
+    // event.target.classList.remove('invalid');
+    changeClass('valid', 'invalid', event.target);
+  } else {
+    // event.target.classList.add('invalid');
+    // event.target.classList.remove('valid');
+    changeClass('invalid', 'valid', event.target);
+  }
+};
+nameInputRef.addEventListener('blur', onInputBlur);
+
+function changeClass(add, remove, elem) {
+  elem.classList.add(add);
+  elem.classList.remove(remove);
+}
