13844ae6c98c2d2c229b58aa8eeccc4cf6c52436
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-08 15:21:24 +0200

    add 03 tags

--- 03-tags.js
+++ 03-tags.js
@@ -3,3 +3,26 @@
  * - один из многих
  * - несколько из многих и Set
  */
+const tagsContainer = document.querySelector('.js-tags');
+
+tagsContainer.addEventListener('click', onTagsContainerClick);
+let selectedTags = null;
+
+function onTagsContainerClick(evt) {
+  if (evt.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  const currentBtnActive = document.querySelector('.tags__btn--active');
+  console.log(currentBtnActive);
+
+  //   console.log(evt.target);
+  const nextActiveBtnTargen = evt.target;
+  nextActiveBtnTargen.classList.add('tags__btn--active');
+  selectedTags = nextActiveBtnTargen.dataset.value;
+
+  if (currentBtnActive) {
+    currentBtnActive.classList.remove('tags__btn--active');
+  }
+  console.log(selectedTags);
+}
