66f4e279b5d1bf35ea627e99759a20c12f51a3a8
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-10 22:48:11 +0200

    calendar finish

--- src/calendar.js
+++ src/calendar.js
@@ -1,1 +1,19 @@
 'use strict';
+const tableRef = document.querySelector('.calendar');
+
+const onCalendarClick = event => {
+  const { target } = event;
+  if (target.nodeName !== 'TD' || target.textContent === '') {
+    return;
+  }
+  const isActive = tableRef.querySelector('.active');
+  if (isActive) {
+    isActive.classList.remove('active');
+  }
+  if (isActive === target) {
+    // isActive.classList.remove('active');
+    return;
+  }
+  target.classList.add('active');
+};
+tableRef.addEventListener('click', onCalendarClick);
