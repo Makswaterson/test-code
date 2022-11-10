f6874d50f7f7d4ab7aedadc776b01108d92d0da7
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-11 01:04:20 +0200

    upd pallet

--- src/pallet.js
+++ src/pallet.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const listItemsRefs = document.querySelectorAll('.pallet__item');
+const listRef = document.querySelector('.js-pallet');
+listItemsRefs.forEach(item => {
+  const randomColor = getRangomColor();
+  item.style.backgroundColor = randomColor;
+  item.dataset.color = randomColor;
+});
+
+const onPalletClick = ({ target }) => {
+  if (target.nodeName === 'UL') {
+    return;
+  }
+  const spanRef = target.firstElementChild;
+  const isActive = listRef.querySelector('.active');
+
+  if (isActive) {
+    isActive.classList.remove('active');
+    isActive.firstElementChild.textContent = '...';
+  }
+  if (isActive === target) {
+    return;
+  }
+
+  spanRef.textContent = target.dataset.color;
+  target.classList.add('active');
+  console.log(target.dataset.color);
+};
+
+listRef.addEventListener('click', onPalletClick);
+
+// Для динамічного створення палітри
+function getRangomColor() {
+  return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+}
+
+function getRandomHex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, '0');
+}
