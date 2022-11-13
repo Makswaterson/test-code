01044fde39b2626c75d6ee772b512104579a1ee6
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-14 00:28:19 +0200

    add lazy load

--- src/03-lazy-load.js
+++ src/03-lazy-load.js
@@ -1,5 +1,16 @@
-/*
- * Ленивая загрузка изображений (концепция)
- * - нативная поддержка
- * - событие загрузки изображения
- */
+// /*
+//  * Ленивая загрузка изображений (концепция)
+//  * - нативная поддержка
+//  * - событие загрузки изображения
+//  */
+
+const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+console.log(lazyImages);
+
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoad);
+});
+function onImageLoad(evt) {
+  console.log('Фото загружено');
+  evt.target.classList.add('appear');
+}
