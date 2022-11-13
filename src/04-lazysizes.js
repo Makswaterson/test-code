33d4ab592eca27d63b3d3115bba139ca58b07cc2
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-14 00:55:55 +0200

    upd

--- src/04-lazysizes.js
+++ src/04-lazysizes.js
@@ -2,3 +2,14 @@
  * Библиотека lazysizes
  * - feature detection
  */
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+console.log(lazyImages);
+
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoad, { once: true });
+});
+function onImageLoad(evt) {
+  console.log('Фото загружено');
+  evt.target.classList.add('appear');
+}
