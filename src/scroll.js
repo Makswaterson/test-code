9a5b5b44618caecded6a878bde1b99ed399e7787
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-15 00:21:12 +0200

    finish scroll

--- src/scroll.js
+++ src/scroll.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const sectionListItemsRef = document.querySelectorAll('.section');
+sectionListItemsRef.forEach(section => {
+  section.style.backgroundColor = getRangomColor();
+});
+
 function getRangomColor() {
   return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
 }
@@ -9,3 +14,15 @@ function getRandomHex() {
     .toString(16)
     .padStart(2, '0');
 }
+
+const onHandleScroll = () => {
+  if (window.pageYOffset > 800) {
+    console.log('1/2 section 4');
+    const animateSectionRef = document.querySelector('.animated-section');
+    animateSectionRef.classList.add('active');
+  }
+};
+
+const onTrottleHandleClick = _.throttle(onHandleScroll, 300);
+
+document.addEventListener('scroll', onTrottleHandleClick);
